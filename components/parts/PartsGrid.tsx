61225b506f8b56ec63a19a98166de727ddbb948c
Amir Imani <imani.030703@gmail.com>
2026-08-21 21:10:39 +0330

    Updated products

--- components/parts/PartsGrid.tsx
+++ components/parts/PartsGrid.tsx
@@ -30,7 +30,7 @@ export default function PartsGrid({ parts }: PartsGridProps) {
           </h2>
         </div>
 
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-2 gap-3 sm:gap-6 lg:grid-cols-3">
           {visibleParts.map((part) => (
             <PartCard key={part.documentId} {...part} />
           ))}
--- components/products/ProductList.tsx
+++ components/products/ProductList.tsx
@@ -23,7 +23,7 @@ export default function ProductsList({
 
   return (
     <>
-      <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-2 gap-3 sm:gap-5 lg:grid-cols-3">
         {visibleProducts.map((product) => (
           <article
             key={product.documentId}
